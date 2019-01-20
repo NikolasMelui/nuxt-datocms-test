e777d87e0d4ace0054ed8c88803e448c7b42d669
nikolasmelui <slak@samaradom.ru>
2019-01-20 20:01:46 +0400

    And new config

--- apollo/config.js
+++ apollo/config.js
@@ -1,6 +1,9 @@
+import 'dotenv/config';
+
 export default context => {
   return {
     httpEndpoint: 'https://graphql.datocms.com',
+    // getAuth: () => '1d8590def2d03e3ac7bd39c6a90810'
     getAuth: () => process.env.APOLLO_API_KEY
   };
 };
